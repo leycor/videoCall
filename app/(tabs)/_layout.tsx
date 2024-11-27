d7ca3fb78a9c8bc9559dbc751ba8b4aa6c9aeb36
Bryan Rivas <bryan.rivas@intelindev.com>
2024-11-27 18:51:22 -0400

    build android

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="calls"
+        options={{
+          title: 'Calls',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
